682dfa7fce6d7e5b558d73d09e0c2f404e65e57f
aniketvhanoli <157479356+aniketvhanoli@users.noreply.github.com>
2026-01-16 20:09:24 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -1,1 +1,10 @@
+// app.js
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from AWS deployed via Azure DevOps 🚀');
+});
+
+app.listen(3000, () => console.log('App running on port 3000'));
 
